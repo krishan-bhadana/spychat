a983992c68961b85889dd3208a48f49e0a939394
krishan bhadana <bhadana.k11@gmail.com>
2017-06-22 02:47:10 +0530

    login and signup both working

--- spychatapp.py
+++ spychatapp.py
@@ -1,49 +1,52 @@
 import shelve
-current_user=0                                #variable which tells which user logged in
-print ("Hello, let's get started.")
+
 db=shelve.open('db.shlf')
-existing_user=raw_input("Are you a existing user ? (Y/N)")
 
 def login():
-    flag=0
-    while flag==0:
+    flag = 0
+    while flag == 0:
         username = raw_input('Username: ')
-        i = 0                                                        # to search the shelve with index from 0 to number of users
+        i = 0  # to search the shelve with index from 0 to number of users
         while i < db.__len__():
-            if db[str(i)]['username']== username:
+            if db[str(i)]['username'] == username:
                 password = raw_input('Password: ')
-                if db[str(i)]['password']==password:
-                    print 'Yor are logged in '+db[str(i)]['username']
-                    flag=1
+                if db[str(i)]['password'] == password:
+                    print 'Yor are logged in ' + db[str(i)]['username']
+                    flag = 1
+                    current_user=i
                     break
-            i=i+1
+                else:
+                    print 'Try again'
+                    continue
+            i = i + 1
+
 def signup():
+    #global total_no_of_users                                   #the variable for total number of users
     flag=0                                                       #flag sets when the account is succesfully created and stops the loop for re-entering a username
 
     while(flag==0):
         username=raw_input('Select a username: ')
         i = 0                                                       #to search the shelve with index from 0 to number of users
         while i<db.__len__():
-            if db[str(i)]['username']==username:
+            temp=db[str(i)]['username']
+            if temp==username:
                 print 'Username already exists, please try again'
                 break
-            i=i+1
-        if db[str(i)]['username'] == username:
+            i=int(i)+1
+        if temp == username:
             continue
         password=raw_input('Create password: ')
-        #age=raw_input('Enter your age:  ')
-        #rating=raw_input('Enter your rating: ')
-        #status=raw_input('Status :')
-        #db[str(db.__len__())]={'username':username,'password':password,'age':age,'rating':rating,'status_messages':status_messages[status],'current_status':0}
+        db[str(db.__len__())]={'username':username,'password':password}
+        #total_no_of_users = int(total_no_of_users) + 1
         print 'account created'
         flag=1
 
-#def start_chat():
-   # print 'here starts the new chat'
+print ("Hello, let's get started.")
+
+existing_user=raw_input("Are you a existing user ? (Y/N)")
 
 if existing_user.upper()=='Y':
     login()
-    #start_chat()
+
 else:
     signup()
-    #start_chat()
