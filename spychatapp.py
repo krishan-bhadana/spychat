a782093c305c0fe1e06cdbcc9839c0fa8e3567f5
krishan bhadana <bhadana.k11@gmail.com>
2017-06-21 23:01:47 +0530

    login method half done

--- spychatapp.py
+++ spychatapp.py
@@ -5,31 +5,34 @@ print ("Hello, let's get started.")
 db=shelve.open('db.shlf')
 d={'username':'krishan','password':'bhadana'}
 db['0']=d
-total_no_of_users=db.__len__()
 existing_user=raw_input("Are you a existing user ? (Y/N)")
 
 def login():
+    flag=0
     username = raw_input('Username: ')
-    password = raw_input('Password: ')
+    while flag==0:
+        i = 0                                                        # to search the shelve with index from 0 to number of users
+        while i < db.__len__():
+            if [str(i)]['username']== username:
+                password = raw_input('Password: ')
+                if db[str(i)]['password']==password:
 
+                    i=i+1
 def signup():
-    global total_no_of_users                                   #the variable for total number of users
     flag=0                                                       #flag sets when the account is succesfully created and stops the loop for re-entering a username
 
     while(flag==0):
         username=raw_input('Select a username: ')
         i = 0                                                       #to search the shelve with index from 0 to number of users
-        while i<int(total_no_of_users):
-            temp=db[str(i)]['username']
-            if temp==username:
+        while i<db.__len__():
+            if db[str(i)]['username']==username:
                 print 'Username already exists, please try again'
                 break
-            i=int(i)+1
-        if temp == username:
+            i=i+1
+        if db[str(i)]['username'] == username:
             continue
         password=raw_input('Create password: ')
-        db[str(total_no_of_users)]={'username':username,'password':password}
-        total_no_of_users = int(total_no_of_users) + 1
+        db[str(db.__len__())]={'username':username,'password':password}
         print 'account created'
         flag=1
 
