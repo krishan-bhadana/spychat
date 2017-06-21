1b83a333dc2c33b82e8ce03228f94805976fdd59
krishan bhadana <bhadana.k11@gmail.com>
2017-06-21 23:14:17 +0530

    login method complete

--- spychatapp.py
+++ spychatapp.py
@@ -1,5 +1,5 @@
 import shelve
-
+current_user                                 #variable which tells which user logged in
 print ("Hello, let's get started.")
 
 db=shelve.open('db.shlf')
@@ -9,15 +9,17 @@ existing_user=raw_input("Are you a existing user ? (Y/N)")
 
 def login():
     flag=0
-    username = raw_input('Username: ')
     while flag==0:
+        username = raw_input('Username: ')
         i = 0                                                        # to search the shelve with index from 0 to number of users
         while i < db.__len__():
             if db[str(i)]['username']== username:
                 password = raw_input('Password: ')
                 if db[str(i)]['password']==password:
-
-                    i=i+1
+                    print 'Yor are logged in '+db[str(i)]['username']
+                    flag=1
+                    break
+            i=i+1
 def signup():
     flag=0                                                       #flag sets when the account is succesfully created and stops the loop for re-entering a username
 
