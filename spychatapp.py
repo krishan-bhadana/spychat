9011180d81fca2b26e750bb217cd4b5f5c65759a
krishan bhadana <bhadana.k11@gmail.com>
2017-06-22 05:53:32 +0530

    select from previous status (slight error)

--- spychatapp.py
+++ spychatapp.py
@@ -67,9 +67,17 @@ def select_status():
 
     if choice.upper()=='N':
         status = raw_input('Status :')
-        #current_user=str(current_user)
         db[str(current_user)]['status_messages'].append(status)
         db[str(current_user)]['current_status']=len(db[str(current_user)]['status_messages'])-1
+
+    if choice.upper() == 'Y':
+        i=0
+        while i<len(db[str(current_user)]['status_messages']):
+            n=i+1
+            print(str(n)+' '+db[str(current_user)]['status_messages'][i])
+            i=i+1
+        input=raw_input('Select from above status')
+        db[str(current_user)]['current_status']=int(input)-1
 
 def start_chat():
 
@@ -77,7 +85,7 @@ def start_chat():
 
     while showmenu==True:
 
-        menu_choices = "What do you want to do? \n 1. Set status update \n 2. Add a friend \n 3. Send a secret message \n 4. Read a secret message \n 5. Read Chats from a user \n 6. Close Application \n"
+        menu_choices = "\n\nWhat do you want to do? \n 1. Set status update  \n 2. Send a secret message \n 3. Read a secret message \n 4. Read Chats from a user \n 5. Close Application \n"
         menu_choice = raw_input(menu_choices)
 
         if len(menu_choice) > 0:
@@ -86,13 +94,10 @@ def start_chat():
             if menu_choice == 1:
                 select_status()
             elif menu_choice == 2:
-                number_of_friends = add_friend()
-                print 'You have %d friends' % (number_of_friends)
-            elif menu_choice == 3:
                 send_message()
-            elif menu_choice == 4:
+            elif menu_choice == 3:
                 read_message()
-            elif menu_choice == 5:
+            elif menu_choice == 4:
                 read_chat_history()
             else:
                 showmenu = False
@@ -102,7 +107,6 @@ print ("Hello, let's get started.")
 existing_user=raw_input("Are you a existing user ? (Y/N)")
 
 if existing_user.upper()=='Y':
-    #print db[str(db.__len__()-1)]['status_messages'][0]               #to print the status message
     login()
     start_chat()
 
