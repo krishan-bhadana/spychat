e71de7bde01adbca46b6df5594d59c296d5a69ee
krishan bhadana <bhadana.k11@gmail.com>
2017-06-28 14:52:23 +0530

    about to make some changes

---
+++ chats.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+class Spy:
+
+    def __init__(self, name, salutation, age, rating):
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.rating = rating
+        self.online = True
+        self.chats = []
+        self.current_status_message = None
+
+
+class ChatMessage:
+
+    def __init__(self,message,sent_by_me):
+        self.message = message
+        self.time = datetime.now()
+        self.sent_by_me = sent_by_me
+
+admin = Spy('admin', 'Mr.', 4.9, 27)
+
+friends = [admin]
--- display_users.py
+++ display_users.py
@@ -8,6 +8,6 @@ class disp_user_class:
         i = 0
         while i < db.__len__():
             n = i + 1
-            print '\n\t\t' + str(n) + ' Username: ' + db[str(i)]['username']+'\n\t\t  Rating: '+db[str(i)]['rating']+'\n\t\t  Status: ' +db[str(i)]['status_messages'][int(db[str(i)]['current_status'])]+ '\n'
+            print '\n\t\t(' + str(n) + ') Username: ' + db[str(i)]['username']+'\n\t\t    Rating: '+db[str(i)]['rating']+'\n\t\t    Status: ' +db[str(i)]['status_messages'][int(db[str(i)]['current_status'])]+ '\n'
             i = i + 1
         print '\n'
